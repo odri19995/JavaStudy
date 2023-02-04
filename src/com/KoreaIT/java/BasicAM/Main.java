24fe25588361317f259ae781d25873903c551499
odri19995@gmail.com <odri19995@gmail.com>
2023-02-04 12:55:08 +0900

    커밋메세지

--- src/com/KoreaIT/java/BasicAM/Main.java
+++ src/com/KoreaIT/java/BasicAM/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java.BasicAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -8,29 +10,47 @@ public class Main {
 		System.out.println("==프로그램 시작==");
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int count = 0;
-		while (true) {
+		List<Article> articles = new ArrayList<>(); // article에 쓴글을 articles에 올리고 싶다.
+
+		while (true) { // 무한루프안에서 명령을 받는 구조
 			System.out.printf("명령어 ) ");
-			String cmd = sc.nextLine();
-			
+			String cmd = sc.nextLine().trim();// trim : 양쪽의 공백을 절삭하겠다.
 
 			if (cmd.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
-		
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다.");
+					continue;
+				}
+				System.out.println("번호    |     내용 ");
+				for (int i = articles.size()-1; i >=0 ; i--) {
+					Article article = articles.get(i); // arrayList 추출시 get 사용
+					System.out.printf("%d, %s\n", article.count, article.title);
+
+				}
+
 			} else if (cmd.equals("article write")) {
 				count++;
+
 				System.out.printf("제목 : ");
-				String cmd1 = sc.nextLine();
+				String title = sc.nextLine();
 				System.out.printf("내용 : ");
-				String cmd2 = sc.nextLine();
-				System.out.printf("%d번글이 생성되었습니다.\n",count);
-				
+				String body = sc.nextLine();
+				System.out.printf("%d번글이 생성되었습니다.\n", count);
+
+				// 객체화
+
+				Article article = new Article(count, title, body);
+
+				articles.add(article);
+
 			} else if (cmd.equals("system exit")) {
 				break;
-				
+
 			} else {
-				System.out.println("존재하지 않는 명령어입니다.");			}
+				System.out.println("존재하지 않는 명령어입니다.");
+			}
 		}
 
 		System.out.println("==프로그램 종료==");
@@ -38,4 +58,17 @@ public class Main {
 		sc.close();
 	}
 
+}
+
+class Article {
+	int count;
+	String title;
+	String body;
+
+	public Article(int count, String title, String body) {
+		this.count = count;
+		this.title = title;
+		this.body = body;
+
+	}// 외부의 정보들이 Article 필드에 채워진다.
 }
