6f1ca41f64db47b9791684985869f2abb19a14bc
odri19995 <odri19995@gmali.com>
2023-02-06 01:53:01 +0900

    main mini change

--- src/com/KoreaIT/java/BasicAM/Main.java
+++ src/com/KoreaIT/java/BasicAM/Main.java
@@ -3,6 +3,9 @@ package com.KoreaIT.java.BasicAM;
 public class Main {
 	public static void main(String[] args) {
 		new App().start();  //App객체를 만들고 그안의 start 버튼을 눌러준다.
+		
+//		App button = new App(); 객체를 변수에 담아주기
+//		button.start();
 	}
 
 }
