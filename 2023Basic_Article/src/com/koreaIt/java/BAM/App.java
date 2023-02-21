46550a8fadc920a10f5807afe38be32f907a9132
odri19995 <odri19995@gmail.com>
2023-02-21 17:14:08 +0900

    articlecontroller 구현

--- 2023Basic_Article/src/com/koreaIt/java/BAM/App.java
+++ 2023Basic_Article/src/com/koreaIt/java/BAM/App.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.koreaIt.java.BAM.controller.ArticleController;
 import com.koreaIt.java.BAM.controller.MemberController;
 import com.koreaIt.java.BAM.dto.Article;
 import com.koreaIt.java.BAM.dto.Member;
@@ -25,11 +26,9 @@ public class App {
 		makeTestData();
 
 		Scanner sc = new Scanner(System.in);
-		
-		MemberController memberController = new MemberController(members ,sc);
 
-		int lastArticleId = 3;
-		
+		MemberController memberController = new MemberController(members, sc);
+		ArticleController articleController = new ArticleController(articles, sc);
 
 		while (true) {
 			System.out.printf("명령어 ) ");
@@ -44,116 +43,18 @@ public class App {
 			}
 
 			if (cmd.equals("article write")) {
-				int id = lastArticleId + 1;
-				lastArticleId = id;
-				String regDate = Util.getNowDateStr();
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
-
-				Article article = new Article(id, regDate, title, body);
-
-				articles.add(article);
-
-				System.out.printf("%d번 글이 생성되었습니다\n", id);
-
+				articleController.doWrite();
 			} else if (cmd.equals("member join")) {
-				
 				memberController.doJoin();
-				
-
 			} else if (cmd.startsWith("article list")) {
-
-				if (articles.size() == 0) {
-					System.out.println("게시글이 없습니다");
-					continue;
-				}
-
-				String searchKeyword = cmd.substring("article list".length()).trim();
-
-				List<Article> printArticles = articles; // articles를 컨트롤 하기 위해서 리모컨을 넘겨줌
-				if (searchKeyword.length() > 0) {
-					System.out.println("검색어 :" + searchKeyword);
-
-					printArticles = new ArrayList<>(); // 내용이 있을때만 리스트를 만들어준다. 진짜 객체 생성
-					for (Article article : articles) {
-						if (article.title.contains(searchKeyword)) {
-							printArticles.add(article);
-						}
-					}
-					if (printArticles.size() == 0) {
-						System.out.println("검색결과가 없습니다.");
-						continue; // continue는 위에 반복문 while문으로 간다.
-					}
-
-				}
-				System.out.println("번호	|	제목    | 조회수");
-				for (int i = printArticles.size() - 1; i >= 0; i--) {
-					Article article = printArticles.get(i);
-					System.out.printf("%d	|	%s|   %d\n", article.id, article.title, article.viewCnt);
-
-				}
-
-			} else if (cmd.startsWith("article detail ")) {
-				String[] cmdBits = cmd.split(" ");
-
-				int id = Integer.parseInt(cmdBits[2]);
-
-				Article foundArticle = getArticleById(id);
-
-				if (foundArticle == null) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
-					continue;
-				}
-				System.out.printf("%d번 게시물은 존재합니다\n", id);
-				System.out.printf("번호 : %d\n", foundArticle.id);
-				System.out.printf("날짜 : %s\n", foundArticle.regDate); // .subString(0,10)
-				System.out.printf("제목 : %s\n", foundArticle.title);
-				System.out.printf("내용 : %s\n", foundArticle.body);
-				System.out.printf("조회수 : %d\n", foundArticle.viewCnt);
-				foundArticle.increaseViewCount();
-
+				articleController.showList(cmd);
+			}else if (cmd.startsWith("article detail ")) {
+				articleController.showDetail(cmd);
 			} else if (cmd.startsWith("article delete ")) {
-				String[] cmdBits = cmd.split(" ");
-
-				int id = Integer.parseInt(cmdBits[2]);
-
-				Article foundArticle = getArticleById(id);
-
-				if (foundArticle == null) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
-					continue;
-				}
-				// size() = 3
-				// index = 0 1 2
-				// id = 1 2 3
-				articles.remove(articles.indexOf(foundArticle));
-
-				System.out.printf("%d번 게시물이 삭제 되었습니다\n", id);
-
+				articleController.doDelete(cmd);
 			} else if (cmd.startsWith("article modify ")) {
-				String[] cmdBits = cmd.split(" ");
-
-				int id = Integer.parseInt(cmdBits[2]);
-				Article foundArticle = getArticleById(id);
-
-				if (foundArticle == null) {
-					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
-					continue;
-				}
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
-
-				foundArticle.title = title;
-				foundArticle.body = body;
-
-				System.out.printf("%d글이 수정되었습니다.\n", id);
-
+				articleController.doModify(cmd);
 			}
-
 			else {
 				System.out.println("없는 명령어입니다"); // 전부 따져봐야해서 밑으로 놓는다.
 			}
@@ -162,18 +63,6 @@ public class App {
 		sc.close();
 		System.out.println("== 프로그램 종료 ==");
 
-	}
-
-
-
-	private Article getArticleById(int id) {
-
-		for (Article article : articles) {
-			if (article.id == id)
-				return article;
-		}
-
-		return null;
 	}
 
 	private void makeTestData() {
---
+++ 2023Basic_Article/src/com/koreaIt/java/BAM/controller/ArticleController.java
@@ -0,0 +1,139 @@
+package com.koreaIt.java.BAM.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.koreaIt.java.BAM.dto.Article;
+import com.koreaIt.java.BAM.util.Util;
+
+public class ArticleController {
+
+	private Scanner sc;
+	private List<Article> articles;
+	int lastArticleId;
+
+	public ArticleController(List<Article> articles, Scanner sc) {
+		this.sc = sc;
+		this.articles = articles;
+		this.lastArticleId = 3;
+	}
+
+	public void doWrite() {
+		int id = lastArticleId + 1;
+		lastArticleId = id;
+		String regDate = Util.getNowDateStr();
+		System.out.printf("제목 : ");
+		String title = sc.nextLine();
+		System.out.printf("내용 : ");
+		String body = sc.nextLine();
+
+		Article article = new Article(id, regDate, title, body);
+
+		articles.add(article);
+
+		System.out.printf("%d번 글이 생성되었습니다\n", id);
+	}
+
+	public void showList(String cmd) {
+
+		if (articles.size() == 0) {
+			System.out.println("게시글이 없습니다");
+			return;// -> 리턴으로 함수를 종료시키되 넘겨주는 값은 없다.
+		}
+
+		String searchKeyword = cmd.substring("article list".length()).trim();
+
+		List<Article> printArticles = articles; // articles를 컨트롤 하기 위해서 리모컨을 넘겨줌
+		if (searchKeyword.length() > 0) {
+			System.out.println("검색어 :" + searchKeyword);
+
+			printArticles = new ArrayList<>(); // 내용이 있을때만 리스트를 만들어준다. 진짜 객체 생성
+			for (Article article : articles) {
+				if (article.title.contains(searchKeyword)) {
+					printArticles.add(article);
+				}
+			}
+			if (printArticles.size() == 0) {
+				System.out.println("검색결과가 없습니다.");
+				return; // continue는 위에 반복문 while문으로 간다.
+			}
+
+		}
+		System.out.println("번호	|	제목    | 조회수");
+		for (int i = printArticles.size() - 1; i >= 0; i--) {
+			Article article = printArticles.get(i);
+			System.out.printf("%d	|	%s|   %d\n", article.id, article.title, article.viewCnt);
+		}
+	}
+	public void showDetail(String cmd) {
+		String[] cmdBits = cmd.split(" ");
+
+		int id = Integer.parseInt(cmdBits[2]);
+
+		Article foundArticle = getArticleById(id);
+
+		if (foundArticle == null) {
+			System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+			return;
+		}
+		System.out.printf("%d번 게시물은 존재합니다\n", id);
+		System.out.printf("번호 : %d\n", foundArticle.id);
+		System.out.printf("날짜 : %s\n", foundArticle.regDate); // .subString(0,10)
+		System.out.printf("제목 : %s\n", foundArticle.title);
+		System.out.printf("내용 : %s\n", foundArticle.body);
+		System.out.printf("조회수 : %d\n", foundArticle.viewCnt);
+		foundArticle.increaseViewCount();
+
+	}
+	public void doDelete(String cmd) {
+		String[] cmdBits = cmd.split(" ");
+
+		int id = Integer.parseInt(cmdBits[2]);
+
+		Article foundArticle = getArticleById(id);
+
+		if (foundArticle == null) {
+			System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+			return;
+		}
+		// size() = 3
+		// index = 0 1 2
+		// id = 1 2 3
+		articles.remove(articles.indexOf(foundArticle));
+
+		System.out.printf("%d번 게시물이 삭제 되었습니다\n", id);
+
+	}
+	public void doModify(String cmd) {
+		String[] cmdBits = cmd.split(" ");
+
+		int id = Integer.parseInt(cmdBits[2]);
+		Article foundArticle = getArticleById(id);
+
+		if (foundArticle == null) {
+			System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+			return;
+		}
+		System.out.printf("제목 : ");
+		String title = sc.nextLine();
+		System.out.printf("내용 : ");
+		String body = sc.nextLine();
+
+		foundArticle.title = title;
+		foundArticle.body = body;
+
+		System.out.printf("%d글이 수정되었습니다.\n", id);
+
+	}
+	private Article getArticleById(int id) {
+
+		for (Article article : articles) {
+			if (article.id == id)
+				return article;
+		}
+
+		return null;
+	}
+
+}
